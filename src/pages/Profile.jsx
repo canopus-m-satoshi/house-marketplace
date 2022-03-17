34aa2c4a7bc8d462ff083d43c810e7e2018d3815
Satoshi Matsushima <>
2022-03-17 23:30:04 +0900

    95. Fetch Listings From Firebase

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { getAuth, updateProfile } from 'firebase/auth'
 import { doc, updateDoc } from 'firebase/firestore'
 import { toast } from 'react-toastify'
 import { db } from '../firebase.config'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 function Profile() {
