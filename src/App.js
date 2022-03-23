be44f4dea63fa4691423a0f77b4768e458168757
Satoshi Matsushima <>
2022-03-23 21:20:47 +0900

    104. Fetch Single Listing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Category from './pages/Category'
 import CreateListing from './pages/CreateListing'
 import Explore from './pages/Explore'
 import ForgotPassword from './pages/ForgotPassword'
+import Listing from './pages/Listing'
 import Navbar from './components/Navbar'
 import Offers from './pages/Offers'
 import PrivateRoute from './components/PrivateRoute'
@@ -29,6 +30,10 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
         </Routes>
         <Navbar />
       </Router>
