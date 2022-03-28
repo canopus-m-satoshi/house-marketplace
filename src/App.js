63f0d4de9668fcd54312dadb1797ce71a7cf885c
Satoshi Matsushima <>
2022-03-29 00:09:00 +0900

    112. Edit Listing Icon

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
-
-import Category from './pages/Category'
-import Contact from './pages/Contact'
-import CreateListing from './pages/CreateListing'
-import Explore from './pages/Explore'
-import ForgotPassword from './pages/ForgotPassword'
-import Listing from './pages/Listing'
+import 'react-toastify/dist/ReactToastify.css'
 import Navbar from './components/Navbar'
-import Offers from './pages/Offers'
 import PrivateRoute from './components/PrivateRoute'
+import Explore from './pages/Explore'
+import Offers from './pages/Offers'
+import Category from './pages/Category'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
+import CreateListing from './pages/CreateListing'
+import EditListing from './pages/EditListing'
+import Listing from './pages/Listing'
+import Contact from './pages/Contact'
 
 import 'react-toastify/dist/ReactToastify.min.css'
 
@@ -31,6 +32,7 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/edit-listing/:listingId" element={<EditListing />} />
           <Route
             path="/category/:categoryName/:listingId"
             element={<Listing />}
