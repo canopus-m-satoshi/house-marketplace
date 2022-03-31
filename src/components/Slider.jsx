e19918eb6dade24f9198517d210af601b910d183
Satoshi Matsushima <>
2022-03-31 23:48:11 +0900

    114. Clear Up Console Warnings

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,7 +6,6 @@ import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/swiper-bundle.css'
 import Spinner from './Spinner'
-import { async } from '@firebase/util'
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 function Slider() {
